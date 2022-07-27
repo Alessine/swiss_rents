48fb9d3ea97a98a39cdc55e3099a7d0c391b97d8
Angela Niederberger <angelaniederberger@protonmail.ch>
2022-07-27 17:23:17 +0200

    update to version 2 of the app script

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_lottie import st_lottie
+import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
@@ -28,61 +29,21 @@ def load_data(raw_data, proc_data):
     return df_small, geojson
 
 
-def set_up_figure(mapbox_token, geojson):
-    go_figure = make_subplots(
-        rows=2,
-        cols=2,
-        specs=[[{"colspan": 2, "type": "mapbox"}, None], [{}, {}]],
-        horizontal_spacing=0.15,
-        vertical_spacing=0.11,
-        subplot_titles=("<b>Location of Listed Apartments</b>",
-                        "<b>Apartments by Size and Rent</b>",
-                        "<b>Apartments per Canton</b>"),
-        column_widths=[2, 1],
-    )
-
-    # Subplot title font size
-    go_figure.layout.annotations[0].update(font_size=24, x=0.175, y=1.01)
-    go_figure.layout.annotations[1].update(font_size=24, x=0.17, y=0.455)
-    go_figure.layout.annotations[2].update(font_size=24, x=0.8, y=0.455)
-
-    # Axis Labels
-    go_figure.update_xaxes(title={"text": "Floor Space (m²)", "font_size": 16}, row=2, col=1)
-    go_figure.update_yaxes(title={"text": "Rent (CHF)", "font_size": 16}, row=2, col=1)
-    go_figure.update_xaxes(title={"text": "Number of Listings", "font_size": 16}, row=2, col=2)
-
-    go_figure.update_layout(
-        margin={"r": 30, "t": 50, "l": 65, "b": 65},
-        height=1200,
-        hovermode="closest",
-        mapbox=dict(
-            accesstoken=mapbox_token,
-            bearing=0,
-            center=go.layout.mapbox.Center(lat=46.8, lon=8.3),
-            pitch=0,
-            zoom=6.7,
-            layers=[{"source": geojson, "type": "line", "line_width": 1}],
-        ),
-        legend=dict(
-            orientation="h",
-            yanchor="top",
-            y=0.54,
-            xanchor="center",
-            x=0.5,
-            font_size=16,
-            itemsizing="constant",
-        ),
-        template="simple_white",
-        barmode="stack",
-    )
-
-    return go_figure
+def set_up_scattermap():
+    colors = np.array(px.colors.sequential.Cividis)[[0, 3, 6, 9]]
+    traces = [
+        "cheapest",
+        "below average",
+        "above average",
+        "most expensive",
+    ]
+    go_figure = go.Figure()
+    return colors, traces, go_figure
 
 
-def list_scattermap_traces(df, colors, trace_names):
-    scattermap_trace_list = []
+def add_scattermap_traces(df, go_figure, colors, traces):
     for cat, df_grouped in df.groupby("Miete_Kategorie"):
-        scattermap_trace_list.append(
+        go_figure.add_trace(
             go.Scattermapbox(
                 lon=df_grouped["lon"],
                 lat=df_grouped["lat"],
@@ -90,17 +51,68 @@ def list_scattermap_traces(df, colors, trace_names):
                 marker=go.scattermapbox.Marker(size=5, color=colors[cat], opacity=0.5),
                 text=df_grouped["hover_strings_scatter"],
                 hovertemplate="%{text}<extra></extra>",
-                name=trace_names[cat],
+                name=traces[cat],
                 legendgroup=str(cat),
             )
         )
-    return scattermap_trace_list
+    return go_figure
 
 
-def list_scatter_traces(df, colors, trace_names):
-    scatter_trace_list = []
+def add_scattermap_layout(go_figure, mapbox_token):
+    go_figure.update_layout(
+        margin={"r": 0, "t": 45, "l": 0, "b": 0},
+        width=1100,
+        height=660,
+        hovermode="closest",
+        mapbox=dict(
+            accesstoken=mapbox_token,
+            bearing=0,
+            center=go.layout.mapbox.Center(lat=46.8, lon=8.3),
+            pitch=0,
+            zoom=7,
+            layers=[{"source": cantons, "type": "line", "line_width": 1}],
+        ),
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            xanchor="center",
+            x=0.5,
+            font_size=16,
+            itemsizing="constant",
+        ),
+        template="simple_white",
+        title={"text": "Location of Listed Apartments",
+               "font_size": 25,
+               "x": 0.0},
+    )
+    return go_figure
+
+
+def build_scattermap(df, mapbox_token):
+    colors, traces, go_figure = set_up_scattermap()
+    go_figure = add_scattermap_traces(df, go_figure, colors, traces)
+    go_figure = add_scattermap_layout(go_figure, mapbox_token)
+    return go_figure
+
+
+def set_up_subplots():
+    colors = np.array(px.colors.sequential.Cividis)[[0, 3, 6, 9]]
+    go_figure = make_subplots(
+        rows=1,
+        cols=2,
+        subplot_titles=(
+            "Apartments by Size and Rent",
+            "Apartments per Canton",
+        ),
+        column_widths=[1.7, 1],
+        horizontal_spacing=0.165,
+    )
+    return go_figure, colors
+
+
+def add_scatter_traces(df, go_figure, colors):
     for cat, df_grouped in df.groupby("Miete_Kategorie"):
-        scatter_trace_list.append(
+        go_figure.add_trace(
             go.Scatter(
                 x=df_grouped["Fläche"],
                 y=df_grouped["Mietpreis_Brutto"],
@@ -108,26 +120,25 @@ def list_scatter_traces(df, colors, trace_names):
                 marker={"color": colors[cat], "opacity": 0.5},
                 text=df_grouped["hover_strings_scatter"],
                 hovertemplate="%{text}<extra></extra>",
-                name=trace_names[cat],
                 legendgroup=str(cat),
                 showlegend=False,
-            )
+            ),
+            row=1,
+            col=1,
         )
-    return scatter_trace_list
+    return go_figure
 
 
-def list_barplot_traces(df, colors, trace_names):
+def add_barplot_traces(df, go_figure, colors):
     df_grouped = (
         df.groupby(["Kanton", "Miete_Kategorie"])
-          .size()
-          .unstack(level=-1)
-          .fillna(0)
-          .sort_values("Kanton", ascending=False)
-          .reset_index()
+        .size()
+        .unstack(level=-1)
+        .fillna(0)
+        .sort_values("Kanton", ascending=False)
+        .reset_index()
     )
     df_grouped["Total_Kanton"] = df_grouped.iloc[:, 1:].sum(axis=1)
-    barplot_trace_list = []
-
     for cat in df["Miete_Kategorie"].unique():
         hover_strings = [
             f"{round(num_per_canton / total_canton * 100, 2)}%"
@@ -135,7 +146,7 @@ def list_barplot_traces(df, colors, trace_names):
                 df_grouped.loc[:, cat], df_grouped["Total_Kanton"]
             )
         ]
-        barplot_trace_list.append(
+        go_figure.add_trace(
             go.Bar(
                 y=df_grouped["Kanton"],
                 x=df_grouped.loc[:, cat],
@@ -143,35 +154,46 @@ def list_barplot_traces(df, colors, trace_names):
                 marker_color=colors[cat],
                 text=hover_strings,
                 hovertemplate="%{text}<extra></extra>",
-                name=trace_names[cat],
                 legendgroup=str(cat),
                 showlegend=False,
-            )
+            ),
+            row=1,
+            col=2,
         )
-    return barplot_trace_list
+    return go_figure
 
 
-def build_combined_figure(df, mapbox_token, geojson):
-    colors = px.colors.qualitative.Bold
+def define_figure_layout(go_figure):
+    go_figure.update_layout(
+        margin={"r": 0, "t": 45, "l": 0, "b": 0},
+        width=875,
+        height=550,
+        hovermode="closest",
+        template="simple_white",
+        barmode="stack",
+    )
+    return go_figure
 
-    trace_names = [
-        "cheapest",
-        "below average",
-        "above average",
-        "most expensive",
-    ]
 
-    # Create lists with traces
-    scattermap_traces = list_scattermap_traces(df, colors, trace_names)
-    scatter_traces = list_scatter_traces(df, colors, trace_names)
-    barplot_traces = list_barplot_traces(df, colors, trace_names)
-
-    # Put them all into one figure
-    go_figure = set_up_figure(mapbox_token, geojson)
-    go_figure.add_traces(scattermap_traces, rows=1, cols=1)
-    go_figure.add_traces(scatter_traces, rows=2, cols=1)
-    go_figure.add_traces(barplot_traces, rows=2, cols=2)
-
+def build_combined_figure(df):
+    go_figure, colors = set_up_subplots()
+    # Scatter plot
+    go_figure = add_scatter_traces(df, go_figure, colors)
+    # Bar plot
+    go_figure = add_barplot_traces(df, go_figure, colors)
+    # Subplot title font size
+    go_figure.layout.annotations[0].update(font_size=25, x=0.12, y=1.01)
+    go_figure.layout.annotations[1].update(font_size=25, x=0.8, y=1.01)
+    # Axis Labels
+    go_figure.update_xaxes(
+        title={"text": "Floor Space (m²)", "font_size": 16}, row=1, col=1
+    )
+    go_figure.update_yaxes(title={"text": "Rent (CHF)", "font_size": 16}, row=1, col=1)
+    go_figure.update_xaxes(
+        title={"text": "Number of Listings", "font_size": 16}, row=1, col=2
+    )
+    # Layout
+    go_figure = define_figure_layout(go_figure)
     return go_figure
 
 
@@ -182,16 +204,12 @@ def convert_df(df):
 
 # App layout
 st.set_page_config(layout="wide")
-
-st.markdown("<h1 style='color: #7F3C8D'>Apartment Listings in Switzerland (2019)</h1>", unsafe_allow_html=True)
-
+st.markdown("<h1 style='color: #00224e'>Apartment Listings in Switzerland (2019)</h1>", unsafe_allow_html=True)
 st.subheader("Objective")
 st.markdown("""With this app you can explore a collection of Swiss apartment listings from 2019 and 
 find out how different factors influence the rent.""")
-
 st.markdown("---")
 st.subheader("Analysis")
-
 left_col, right_col = st.columns([3, 1])
 left_col.markdown("""The listings were categorized based on the rent per square meter of floor space. 
 You can calculate this indicator for your own apartment and see in what category it falls.
@@ -220,7 +238,6 @@ with right_col.form("Rent/m²", clear_on_submit=True):
         else:
             rent_per_m2 = user_rent / user_floor_space
             st.write(f"Rent/m²: CHF {round(rent_per_m2, 2)}")
-
 st.text("")
 
 # User dependent variables
@@ -236,7 +253,7 @@ df_plotting = deepcopy(df_proc)
 
 # Sidebar
 # Lottie icon
-lottie_url = "https://assets10.lottiefiles.com/packages/lf20_7ttkwwdk.json"
+lottie_url = "https://assets3.lottiefiles.com/packages/lf20_fgne1q0e.json"
 lottie_pin = load_lottieurl(lottie_url)
 with st.sidebar:
     st_lottie(lottie_pin, speed=1, height=100)
@@ -264,9 +281,12 @@ with st.sidebar.form("Selection Criteria"):
 
 try:
     assert len(df_plotting.index) > 0, "Dataframe is empty."
-    # Plotly Combined Plot
-    joint_fig = build_combined_figure(df=df_plotting, mapbox_token=mapbox_access_token, geojson=cantons)
+    map_fig = build_scattermap(df=df_plotting, mapbox_token=mapbox_access_token)
+    st.plotly_chart(map_fig, use_container_width=True)
+
+    joint_fig = build_combined_figure(df=df_plotting)
     st.plotly_chart(joint_fig, use_container_width=True)
+
 except AssertionError:
     st.sidebar.write("There are no apartment listings that meet these criteria.")
     df_plotting = deepcopy(df_proc)
